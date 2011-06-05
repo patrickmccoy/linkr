cc0a12d4918f21022a1ba54b1001dc6ad29e95dd
Patrick McCoy <patrick@pjmccoy.com>
2011-06-05 13:57:03 -0500

    added client-side javascript to open new links and remove the clicked link from the list

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,16 @@
 */
 
 
-
+$('.linkr_link').click(function(e){
+	e.preventDefault();
+	
+	var container = $(this).parent().parent(),
+		location = $(this).attr('href');
+	
+	container.remove();
+	window.open(location);
+	
+});
 
 
 
