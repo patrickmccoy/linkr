eab02914ac9a26f0bd271101a8766023a62cbefe
Patrick McCoy <patrick@pjmccoy.com>
2011-06-16 02:20:22 -0500

    added modal popup window for add links

--- public/js/script.js
+++ public/js/script.js
@@ -37,6 +37,46 @@ var niceTime = function(timestamp) {
 	return month+'/'+day+'/'+year+' '+hour+':'+min+' '+meridian;
 }
 
+
+/**
+ * Render and show a modal window with an add link form in it
+ */
+var showAddLink = function() {
+
+	
+
+	var form = $('<form>').attr('id','add_link_ajax'),
+		method = $('<input>').attr('type','hidden').attr('name','_method').val('post'),
+		title = $('<p>').html('Add a link to read later:'),
+		url_container = $('<p>').html('<label for="url">URL: </label>'),
+		url = $('<input>').attr('id','url').attr('type','url').attr('name','url').attr('required','required').attr('placeholder','http://www.google.com');
+		
+	url_container.append(url);
+	
+	form.append(method)
+		.append(title)
+		.append(url_container);
+
+	/* add the submit functionality to the form */
+	form.submit(function(e){
+			e.preventDefault();
+			
+			$.ajax({
+				type: 'POST',
+				url: '/api/link',
+				data: $(this).serialize(),
+				success: function(data) {
+					var link = renderLink(data);
+					modal_add.hideAndUnload(addLinkToPage(link));
+				},
+				dataType: 'json'
+			});
+		});
+	
+	var modal_add = new Boxy(form, { title: 'Add a link', modal: true });
+
+}
+
 /**
  * Render a link from JSON and return a jQuery object of that rendered link for placing on the page
  */
@@ -53,10 +93,16 @@ var renderLink = function(link) {
 	
 	/* Put it all together */
 	link_container.append(link);
-	container.append(time);
-	container.append(link_container);
+	container.append(time)
+			 .append(link_container);
 	
 	return container;
+}
+
+var addLinkToPage = function(renderedLink) {
+	var linkContainer = $('div#links');
+	
+	linkContainer.append(renderedLink);
 }
 
 
@@ -71,7 +117,14 @@ $('.linkr_link').click(function(e){
 	
 });
 
-$('form#add_link').submit(function(e){
+$('a#add_link').click(function(e){
+	e.preventDefault();
+	
+	/* open a new boxy with the add link form in it */
+	showAddLink();
+});
+
+$('form#add_link_ajax').submit(function(e){
 	e.preventDefault();
 	
 	$.ajax({
