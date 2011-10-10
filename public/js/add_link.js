37c0386d7f8ed65f17d633df4c212c6819a64f80
Patrick McCoy <patrick@webdev01.swixr.com>
2011-10-10 14:48:35 +0000

    fixed base uri in bookmarklet scripts to point to production

--- public/js/add_link.js
+++ public/js/add_link.js
@@ -1,5 +1,5 @@
 (function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,"1.6.1",function($,L){
-var uri_base = 'http://localhost:3001';
+var uri_base = 'http://linkr.cc';
 $.ajax({
 	type: "GET",
 	url: uri_base+'/api/bookmarklet_add',
